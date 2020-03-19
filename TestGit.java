b1d281649691e1ad8b89ac39db171b3dda988143
pislarucelena <57871165+pislarucelena@users.noreply.github.com>
2020-03-19 21:11:23 +0200

    Create TestGit.java

---
+++ TestGit.java
@@ -0,0 +1,10 @@
+package temaCTS;
+
+public class TestGit  {
+	
+	  public static void main(String[] args) {
+	    	
+	            System.out.println("Hello Git ! Denumirea proiectului de licenta este: Sistem software pentru managementul contabilității unei firme."  );
+	  }
+	
+}
