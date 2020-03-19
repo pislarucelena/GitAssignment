3ef296ff2615c11598c95e9e29b67129c523bd6e
pislarucelena <57871165+pislarucelena@users.noreply.github.com>
2020-03-19 21:12:27 +0200

    Create Angajat.java
    Am creat clasa Angajat care contine 3 atribute si un constructor.

---
+++ Angajat.java
@@ -0,0 +1,16 @@
+package temaCTS;
+
+public class Angajat implements DetaliiAngajat {
+
+	private String nume;
+	private int vechime;
+	private String functie;
+	
+	public Angajat() {
+		super();
+		this.nume = nume;
+		this.vechime = vechime;
+		this.functie = functie;
+	}
+	
+}
