76e551f0aa264f73a899276a5e7c476555e1b0f8
pislarucelena <57871165+pislarucelena@users.noreply.github.com>
2020-03-19 21:20:10 +0200

    Update TestGit.java

--- TestGit.java
+++ TestGit.java
@@ -5,6 +5,11 @@ public class TestGit  {
 	  public static void main(String[] args) {
 	    	
 	            System.out.println("Hello Git ! Denumirea proiectului de licenta este: Sistem software pentru managementul contabilității unei firme."  );
+	       	
+	            
+	 DetaliiAngajat op = new Angajat();
+     op.functieAngajat("Popescu Vasile", "Contabil Sef");
+     op.vechimeAngajat(5);
 	  }
 	
 }
